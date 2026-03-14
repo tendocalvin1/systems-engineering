0303a13bf13a21e7ba082e7387e17203150b7c05
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-03-14 21:53:31 +0300

    I have added the end points for creating a user in a basic authentication system

--- backend-folder/src/routes/user.routes.js
+++ backend-folder/src/routes/user.routes.js
@@ -1,5 +1,7 @@
 
 import { Router } from "express";
+import { registerUser } from "../controllers/user.controller";
 const router = Router();
 
+router.post('/register').post(registerUser);
 export default router;
