773a086957a0d9d6da9ed2bfd094b729057d1c3a
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-03-15 10:26:02 +0300

    Corrected how to create a route for the user by calling the Router functionality from express.js

--- backend-folder/src/app.js
+++ backend-folder/src/app.js
@@ -2,4 +2,17 @@ import express from 'express';
 
 const app = express();  // create an express app
 
+app.use(express.json())
+
+// routes import
+import UserRouter from './routes/user.routes.js';
+
+
+// routes declaration
+app.use('/api/v1/users', UserRouter)
+
+
+// example route: http://localhost:8000/api/v1/users/register
+
+
 export default app;
