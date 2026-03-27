c10140012d6003f82ee29ed76c7923abf133eeed
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-03-27 07:01:19 +0300

    Included the logout process as well to make the system more functional

--- backend-folder/src/controllers/user.controller.js
+++ backend-folder/src/controllers/user.controller.js
@@ -51,7 +51,7 @@ const loginUser = async (req, res) => {
 
      if(!user) return res.status(404).json({message: "User not found"})
 
-    // checking and comparing passwords
+    // checking and comparing passwords 
 
     const isMatch = await user.comparePassword(password);
     if(!isMatch) return res.status(400).json({message: "Invalid credentials"})
@@ -70,5 +70,23 @@ const loginUser = async (req, res) => {
 }
 
 
+const logoutUser = async (req, res) => {
+    try {
+        const { email } = req.body
+        const user = await User.findOne({
+            email
+        })
 
-export { registerUser, loginUser }
+        if(!user) return res.status(404).json({message: "User not found!"})
+
+        res.status(200).json({message: "Logout successful!"})
+
+
+    } catch (error) {
+        res.status(500).json({message: "Internal server error", error: error.message})
+    }   
+}
+
+
+
+export { registerUser, loginUser, logoutUser }
